47c7876aceeace6b8fc008334bb269554b4b27d6
nchalifo <nchalifo@uoguelph.ca>
2018-11-15 19:45:29 -0500

    revert

--- train.py
+++ train.py
@@ -25,9 +25,11 @@ if __name__ == '__main__':
     tb_thread.daemon = True
     tb_thread.start()
 
-    print('Loading data...')
+    train_graph = tf.Graph()
+    eval_graph = tf.Graph()
+    infer_graph = tf.Graph()
 
-    graph = tf.Graph()
+    print('Loading data...')
 
     output_mapping = utils.load_output_mapping('data/output_space.txt')
     hparams.n_classes = len(output_mapping) + 1  # not entirely sure we +1 here
@@ -43,18 +45,28 @@ if __name__ == '__main__':
 
     print('Initializing model...')
 
-    with graph.as_default():
+    with train_graph.as_default():
 
-        model = Model(hparams, ModelModes.TRAIN)
+        train_model = Model(hparams, ModelModes.TRAIN)
         variables_initializer = tf.global_variables_initializer()
 
-    train_writer = tf.summary.FileWriter(os.path.join(hparams.logdir, 'train'), graph=graph)
-    eval_writer = tf.summary.FileWriter(os.path.join(hparams.logdir, 'eval'), graph=graph)
+    with eval_graph.as_default():
 
-    sess = tf.Session(graph=graph,
-                      config=tf.ConfigProto(log_device_placement=hparams.log_device_placement))
+        eval_model = Model(hparams, ModelModes.EVAL)
 
-    sess.run(variables_initializer)
+    with infer_graph.as_default():
+
+        infer_model = Model(hparams, ModelModes.INFER)
+
+    train_writer = tf.summary.FileWriter(os.path.join(hparams.logdir, 'train'), graph=train_graph)
+    eval_writer = tf.summary.FileWriter(os.path.join(hparams.logdir, 'eval'), graph=eval_graph)
+
+    train_sess = tf.Session(graph=train_graph,
+                            config=tf.ConfigProto(log_device_placement=hparams.log_device_placement))
+    eval_sess = tf.Session(graph=eval_graph)
+    infer_sess = tf.Session(graph=infer_graph)
+
+    train_sess.run(variables_initializer)
 
     epoch = hparams.n_epochs
     batch_size = hparams.batch_size
@@ -68,7 +80,7 @@ if __name__ == '__main__':
 
     checkpoints_path = os.path.join(checkpoints_path, 'checkpoint')
 
-    model.save('model', sess, global_step=0)
+    train_model.save('model', train_sess, global_step=0)
 
     print('Training...')
 
@@ -83,7 +95,7 @@ if __name__ == '__main__':
                 batch_train_x = np.asarray(x_train[i*batch_size:(i+1)*batch_size], dtype=np.float32)
                 batch_train_y = utils.sparse_tuple_from(np.asarray(y_train[i * batch_size:(i + 1) * batch_size]))
 
-                cost, _, summary = model.train(batch_train_x, batch_train_y, sess)
+                cost, _, summary = train_model.train(batch_train_x, batch_train_y, train_sess)
 
                 global_step += batch_size
 
@@ -91,32 +103,36 @@ if __name__ == '__main__':
 
                 print('epoch: {}, global_step: {}, cost: {}, time: {}'.format(current_epoch, global_step, cost, time.time() - start_time))
 
-                # if global_step % steps_per_checkpoint == 0:
-                #
-                #     print('checkpointing... (global step = {})'.format(global_step))
-                #
-                #     checkpoint_path = train_model.saver.save(train_sess, checkpoints_path, global_step=global_step)
-                #     eval_model.saver.restore(eval_sess, checkpoint_path)
-                #     infer_model.saver.restore(infer_sess, checkpoint_path)
-                #
-                #     ler, summary = eval_model.eval(batch_train_x, batch_train_y, eval_sess)
-                #
-                #     eval_writer.add_summary(summary, global_step=global_step)
-                #
-                #     print('Eval --- LER: {} %'.format(ler*100))
-                #
-                #     decoded_ids = infer_model.infer([batch_train_x[0]], infer_sess)[0][0].values
-                #
-                #     original_text = utils.ids_to_text(y_train[i*batch_size], output_mapping)
-                #     decoded_text = utils.ids_to_text(decoded_ids, output_mapping)
-                #
-                #     print('GROUND TRUTH: {}'.format(original_text))
-                #     print('PREDICTION: {}'.format(decoded_text))
+                if global_step % steps_per_checkpoint == 0:
+
+                    print('checkpointing... (global step = {})'.format(global_step))
+
+                    checkpoint_path = train_model.saver.save(train_sess, checkpoints_path, global_step=global_step)
+                    eval_model.saver.restore(eval_sess, checkpoint_path)
+                    infer_model.saver.restore(infer_sess, checkpoint_path)
+
+                    ler, summary = eval_model.eval(batch_train_x, batch_train_y, eval_sess)
+
+                    eval_writer.add_summary(summary, global_step=global_step)
+
+                    print('Eval --- LER: {} %'.format(ler*100))
+
+                    decoded_ids = infer_model.infer([batch_train_x[0]], infer_sess)[0][0].values
+
+                    original_text = utils.ids_to_text(y_train[i*batch_size], output_mapping)
+                    decoded_text = utils.ids_to_text(decoded_ids, output_mapping)
+
+                    print('GROUND TRUTH: {}'.format(original_text))
+                    print('PREDICTION: {}'.format(decoded_text))
 
             if epoch > 0: epoch -= 1
 
     except KeyboardInterrupt:
 
-        sess.close()
+        train_sess.close()
+        eval_sess.close()
+        infer_sess.close()
 
-    sess.close()
+    train_sess.close()
+    eval_sess.close()
+    infer_sess.close()
